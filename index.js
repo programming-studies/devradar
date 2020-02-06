ce9edfae6cde4237fa9dd3003abf947f07a332f0
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-02-05 22:20:48 -0300

    adicionada conexão com o mongodb por meio do mongoose

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = '3000' } = process.env
 const HOST = '0.0.0.0';
 
 const app = express();
 
+mongoose.connect('mongodb+srv://usu_omnistack:pwd_omnistack@fish-xxoc1.mongodb.net/omnistack-10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
-    /**/
     return res.send('Hello World');
-    /*/
-    return res.send('Hello World 2asdaushd');
-    /**/
 });
 
 app.listen(PORT, HOST);
