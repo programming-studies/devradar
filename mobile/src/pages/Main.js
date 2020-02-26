e7122686af1894160560d0735d77646c6d3c1749
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2020-02-26 03:39:18 -0300

    formulário para buscar desenvolvedores na tela do mapa

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
 } from "expo-location";
-import { addListener } from "expo/build/Updates/Updates";
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -39,33 +46,52 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView style={styles.map} initialRegion={currentRegion}>
-      <Marker coordinate={{ latitude: -7.0996074, longitude: -34.8392131 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://avatars0.githubusercontent.com/u/5747855?s=60&v=4"
-          }}
+    <>
+      <MapView style={styles.map} initialRegion={currentRegion}>
+        <Marker coordinate={{ latitude: -7.0996074, longitude: -34.8392131 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars0.githubusercontent.com/u/5747855?s=60&v=4"
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "mikhailcavalcanti"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Mikhail Cavalcanti</Text>
+              <Text style={styles.devBio}>
+                Programador entusiasta de novas tecnologias
+              </Text>
+              <Text style={styles.devTechs}>
+                Node Js, React Native, React, Php
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            navigation.navigate("Profile", {
-              github_username: "mikhailcavalcanti"
-            });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Mikhail Cavalcanti</Text>
-            <Text style={styles.devBio}>
-              Programador entusiasta de novas tecnologias
-            </Text>
-            <Text style={styles.devTechs}>
-              Node Js, React Native, React, Php
-            </Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons
+            // style={{ justifyContent: "center", alignContent: "center" }}
+            name="my-location"
+            size={20}
+            color="#fff"
+          />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -91,6 +117,36 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 4
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 4, height: 4 },
+    elevation: 5
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8e4dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
   }
 });
 
